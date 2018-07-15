e98ab628d5b824e34c8a5fd047ab1e426961657a
Chris <chriswingler1986@gmail.com>
2018-07-14 18:00:46 -0700

    finish part 1 of exercise

--- students/chriswingler/main.go
+++ students/chriswingler/main.go
@@ -8,32 +8,62 @@ import (
 )
 
 func main() {
+	// whitespace formatting
+	addLineBreak(1)
+
+	fmt.Println("It's quiz time~!")
+
+	addLineBreak(2)
+
+	// open csv file and handle potential errors
 	csvFile, err := os.Open("../../problems.csv")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// instantiate a new csv reader
 	csvReader := csv.NewReader(csvFile)
 
+	// read all lines in csv and handle potential errors
 	line, err := csvReader.ReadAll()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for i := 0; i < len(line); i++ {
-		fmt.Println(line[i][0])
-		fmt.Println(line[i][1])
-	}
-
+	// set val with type string to handle cmd line input
 	var val string
 
-	for {
-		fmt.Println("enter a value")
+	// loop through each line and get input / compare to answer
+	for i := range line {
+		fmt.Println("What is", line[i][0], "?")
+
+		addLineBreak(1)
 
 		fmt.Scanf("%s", &val)
 
-		fmt.Println("you entered: ", val)
+		addLineBreak(1)
+
+		var greeting string
+
+		if val == line[i][1] {
+			greeting = ":) Congrats!"
+		} else {
+			greeting = ":( Aww.."
+		}
+
+		addLineBreak(5)
+
+		fmt.Println(greeting, "You entered:", val, "and the answer is", line[i][1])
+
+		addLineBreak(1)
+
+	}
+}
+
+func addLineBreak(n int) {
+	for i := 0; i <= n; i++ {
+		fmt.Println()
 	}
 }
